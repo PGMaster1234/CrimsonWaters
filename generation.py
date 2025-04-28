daf57a0721334f46c8fccda46acceea7faca8d93
Pranav <pxg082007@gmail.com>
2025-04-27 22:35:56 -0500

    Map generation, tile coloring, and territory locational spawning

---
+++ generation.py
@@ -0,0 +1,253 @@
+import pygame
+import math
+import random
+from text import drawText
+from calcs import distance, ang, normalize_angle, draw_arrow, linearGradient, normalize, randomCol
+
+
+class TileHandler:
+    def __init__(self, width, height, size, cols, waterThreshold=0.51, mountainThreshold=0.51, territorySize=100, font=None):
+        self.surf = pygame.Surface((width, height)).convert_alpha()
+        self.debugOverlay = pygame.Surface((width, height)).convert_alpha()
+        self.surf.fill(cols.dark)
+        self.debugOverlay.fill(cols.dark)
+
+        self.size = size
+        self.tiles = []
+        self.territorySize = territorySize
+        self.contiguousTerritories = []
+        self.font = font
+        self.cols = cols
+        self.shifter = [0, 0]
+        self.shifterMomentum = [0, 0]
+        self.waterThreshold = waterThreshold
+        self.mountainThreshold = mountainThreshold
+
+        self.borderSize = 4
+        self.horizontal_distance = (3 / 2 * size)
+        self.vertical_distance = (math.sqrt(3) * size)
+        self.gridSizeX = int(width / self.horizontal_distance) - self.borderSize - 1
+        self.gridSizeY = int(height / self.vertical_distance) - self.borderSize
+
+        self.generateTiles()
+        self.assignAdjacentTiles()
+        for _ in range(50):
+            self.generationCycle()
+        self.landRegions = self.findContiguousRegions([t for t in self.tiles if t.waterLand >= self.waterThreshold])
+        self.mountainRegions = self.findContiguousRegions([t for t in self.tiles if (t.waterLand >= self.waterThreshold and t.mountainous > self.mountainThreshold)])
+        self.setRegionReferences()
+        self.createTerritories()
+
+        self.setTileCols()
+        self.draw2InternalScreen()
+
+    def generateTiles(self):
+        for x in range(self.gridSizeX):
+            for y in range(self.gridSizeY):
+                x_pos = self.horizontal_distance * x + self.size * (self.borderSize + 0.5)
+                y_pos = self.vertical_distance * y + self.size * (self.borderSize + 0.5)
+                if x % 2 == 1:
+                    y_pos += self.vertical_distance / 2
+                self.tiles.append(Hex(x, y, x_pos, y_pos, self.size))
+
+    def assignAdjacentTiles(self):
+        grid_dict = {(tile.grid_x, tile.grid_y): tile for tile in self.tiles}
+
+        for tile in self.tiles:
+            tile.adjacent = []
+
+            if tile.grid_x % 2 == 0:
+                offsets = [(1, 0), (-1, 0), (0, 1), (0, -1), (-1, -1), (1, -1)]
+            else:
+                offsets = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1)]
+
+            for dx, dy in offsets:
+                neighbor_grid_x = tile.grid_x + dx
+                neighbor_grid_y = tile.grid_y + dy
+
+                if (neighbor_grid_x, neighbor_grid_y) in grid_dict:
+                    tile.adjacent.append(grid_dict[(neighbor_grid_x, neighbor_grid_y)])
+
+    def getTileAtPosition(self, x, y):
+        for tile in self.tiles:
+            if math.isclose(tile.x, x, abs_tol=self.horizontal_distance / 2) and math.isclose(tile.y, y, abs_tol=self.vertical_distance / 2):
+                return tile
+        return None
+
+    def generationCycle(self):
+        waterLandShifts = [sum([adj.waterLand for adj in tile.adjacent]) / len(tile.adjacent) for tile in self.tiles]
+        mountainShifts = [sum([adj.mountainous for adj in tile.adjacent]) / len(tile.adjacent) for tile in self.tiles]
+        for i, tile in enumerate(self.tiles):
+            tile.waterLand += (waterLandShifts[i] - tile.waterLand) / 2
+            tile.mountainous += (mountainShifts[i] - tile.mountainous) / 2
+
+    def setTileCols(self):
+        waterTiles = [(tile.waterLand if tile.waterLand < self.waterThreshold else 0.5) for tile in self.tiles]
+        landTiles = [(tile.waterLand if tile.waterLand >= self.waterThreshold else 0.5) for tile in self.tiles]
+        mountainTiles = [(tile.mountainous if (tile.waterLand >= self.waterThreshold and tile.mountainous >= self.mountainThreshold) else 0.5) for tile in self.tiles]
+        waterBounds = [min(waterTiles), max(waterTiles)]
+        landBounds = [min(landTiles), max(landTiles)]
+        mountainBounds = [min(mountainTiles), max(mountainTiles)]
+        waterColoringNoise = 0.006
+        landColoringNoise = 0.004
+        mountainColoringNoise = 0.01
+
+        for i, tile in enumerate(self.tiles):
+            weightWaterDistribution = lambda x: (x ** 2) / 2 + (1 - (1 - x) ** 2) ** 10 / 2
+            if tile.waterLand < self.waterThreshold:
+                tile.col = linearGradient([self.cols.oceanBlue, self.cols.oceanGreen, self.cols.lightOceanGreen, self.cols.oceanFoam], weightWaterDistribution(normalize(tile.waterLand + random.uniform(-waterColoringNoise, waterColoringNoise), waterBounds[0], waterBounds[1], True)))
+            elif tile.waterLand >= self.waterThreshold:
+                weightLandDistribution = lambda x: (1 - 2 ** (-3 * x)) * 8 / 7
+                tile.isLand = True
+                if tile.mountainous < self.mountainThreshold:
+                    tile.col = linearGradient([self.cols.oliveGreen, self.cols.darkOliveGreen], weightLandDistribution(normalize(tile.waterLand + random.uniform(-landColoringNoise, landColoringNoise), landBounds[0], landBounds[1], True)))
+                else:
+                    tile.isMountain = True
+                    tile.col = linearGradient([self.cols.mountainBlue, self.cols.darkMountainBlue], normalize(tile.mountainous + random.uniform(-mountainColoringNoise, mountainColoringNoise), mountainBounds[0], mountainBounds[1], True))
+
+    @staticmethod
+    def findContiguousRegions(tiles):
+        visited = set()
+        regions = []
+
+        for tile in tiles:
+            if tile in visited:
+                continue
+            stack = [tile]
+            region = []
+
+            # DFS
+            while stack:
+                current = stack.pop()
+                if current in visited:
+                    continue
+                visited.add(current)
+                region.append(current)
+                for adj in current.adjacent:
+                    if (adj in tiles) and (adj not in visited):
+                        stack.append(adj)
+            regions.append(region)
+        print(f"Found {len(regions)} contiguous regions of sizes {[len(reg) for reg in regions]}")
+        return regions
+
+    def setRegionReferences(self):
+        for region in self.landRegions:
+            col = randomCol('g')
+            for tile in region:
+                tile.region = region
+                tile.regionCol = col
+        for region in self.mountainRegions:
+            col = randomCol('r')
+            for tile in region:
+                tile.mountainRegion = region
+                tile.regionCol = col
+
+    @staticmethod
+    def iterate(points, centers):
+        # p = [x, y, distance, parent, tileReference]
+        # c = [x, y, points, tileReferences]
+        for p in points:
+            p[2] = 1e9
+            p[3] = None
+
+        for c in centers:
+            c[2] = []
+            for p in points:
+                d = distance((p[0], p[1]), (c[0], c[1]))
+                if d < p[2]:
+                    c[2].append(p)
+                    if p[3] is not None:
+                        p[3][2].remove(p)
+                    p[3] = c
+                    p[2] = d
+        return points, centers
+
+    @staticmethod
+    def shiftCenters(centers):
+        # p = [x, y, distance, parent, tileReference]
+        # c = [x, y, points, tileReferences]
+        for c in centers:
+            if len(c[2]) > 0:
+                total_x = sum(p[0] for p in c[2])
+                total_y = sum(p[1] for p in c[2])
+                c[0] = total_x / len(c[2])
+                c[1] = total_y / len(c[2])
+        return centers
+
+    def createTerritories(self):
+        # p = [x, y, distance, parent, tileReference]
+        # c = [x, y, points, tileReferences]
+        self.contiguousTerritories = []
+        for region in self.landRegions:
+            points = [[tile.center[0], tile.center[1], 1e9, None, tile] for tile in region]
+            centers = [[p[0], p[1], [], []] for p in random.sample(points, math.ceil(len(region) / self.territorySize))]
+            for _ in range(10):
+                points, centers = self.iterate(points, centers)
+                centers = self.shiftCenters(centers)
+            for p in points:
+                p[3][3].append(p[4]) # store tile reference
+            self.contiguousTerritories.append([[c[0], c[1]], c[3]] for c in centers)
+
+    def findBoundaryTiles(self, territoryTiles):
+        boundary = []
+        for tile in territoryTiles:
+            for adj in tile.adjacent:
+                if adj not in territoryTiles:
+                    boundary.append(tile)
+                    break
+        for tile in boundary:
+            tile.col = self.cols.light
+
+    def draw2InternalScreen(self):
+        for tile in self.tiles:
+            tile.draw(self.surf, False)
+            tile.draw(self.debugOverlay, True)
+        for contiguousTerritoryList in self.contiguousTerritories:
+            for territory in contiguousTerritoryList:
+                self.findBoundaryTiles(territory[1])
+                pygame.draw.circle(self.surf, self.cols.dark, territory[0], 4, 2)
+                pygame.draw.circle(self.debugOverlay, self.cols.dark, territory[0], 4, 2)
+
+    def draw(self, s, showArrows=False, showDebugOverlay=False, showWaterLand=False):
+        s.blit(self.surf, (0, 0))
+        if showDebugOverlay:
+            s.blit(self.debugOverlay, (0, 0))
+        if showArrows:
+            for tile in self.tiles:
+                tile.drawArrows(s)
+        if showWaterLand:
+            for tile in self.tiles:
+                tile.showWaterLand(s, self.font)
+
+
+class Hex:
+    def __init__(self, grid_x, grid_y, x, y, size, col=(0, 0, 0)):
+        self.grid_x = grid_x
+        self.grid_y = grid_y
+        self.x = x
+        self.y = y
+        self.size = size
+        self.col = col
+        self.center = [self.x + self.size / 2, self.y + self.size / 2]
+        self.adjacent = []
+        self.waterLand = random.randint(0, 1)
+        self.mountainous = random.randint(0, 1)
+        self.isLand = False
+        self.isMountain = False
+        self.region = None
+        self.mountainRegion = None
+        self.regionCol = None
+
+    def draw(self, s, showRegions=False):
+        points = [(self.x + self.size * math.cos(math.pi / 3 * angle), self.y + self.size * math.sin(math.pi / 3 * angle)) for angle in range(6)]
+        pygame.draw.polygon(s, self.regionCol if self.isLand and showRegions else self.col, points)
+
+    def drawArrows(self, s):
+        for adj in self.adjacent:
+            angle = normalize_angle(ang((self.x, self.y), (adj.x, adj.y)))
+            dist = distance((self.x, self.y), (adj.x, adj.y))
+            factor = 0.35
+            draw_arrow(s, (self.x, self.y), (self.x + dist * factor * math.cos(angle), self.y + dist * factor * math.sin(angle)), self.col.debugRed, pygame, 2, 5, 25)
+
+    def showWaterLand(self, s, font):
+        drawText(s, self.col.dark, font, self.x - self.size / 2, self.y - self.size / 2, str(round(self.waterLand, 2)))
---
+++ main_screen.py
@@ -0,0 +1,115 @@
+import pygame
+import math
+import time
+from text import drawText
+from fontDict import fonts
+from generation import TileHandler
+
+pygame.init()
+
+# ---------------- Setting up the screen, assigning some global variables, and loading text fonts
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+fps = 60
+scaleDownFactor = 2
+screen_width = int(screen.get_width() / scaleDownFactor)
+screen_height = int(screen.get_height() / scaleDownFactor)
+screen_center = [screen_width / 2, screen_height / 2]
+screen2 = pygame.Surface((screen_width, screen_height)).convert_alpha()
+screenT = pygame.Surface((screen_width, screen_height)).convert_alpha()
+screenT.set_alpha(100)
+screenUI = pygame.Surface((screen_width, screen_height)).convert_alpha()
+timer = 0
+shake = [0, 0]
+shake_strength = 3
+montserratRegularAdaptive = fonts[f"regular{int(25 / (scaleDownFactor ** (1 / 1.5)))}"]
+montserratExtralightAdaptive = fonts[f"extralight{int(25 / (scaleDownFactor ** (1 / 1.5)))}"]
+montserratBoldAdaptive = fonts[f"bold{int(25 / (scaleDownFactor ** (1 / 1.5)))}"]
+montserratThinAdaptive = fonts[f"thin{int(25 / (scaleDownFactor ** (1 / 1.5)))}"]
+
+montserratRegular15 = fonts[f"regular{int(15 / (scaleDownFactor ** (1 / 1.5)))}"]
+
+
+class Cols:
+    oceanBlue = [59, 95, 111]
+    oceanGreen = [73, 120, 122]
+    lightOceanGreen = [86, 142, 143]
+    oceanFoam = [148, 182, 180]
+
+    sandyBrown = [172, 146, 95]
+    darkSandyBrown = [159, 143, 91]
+
+    oliveGreen = [95, 115, 84]
+    darkOliveGreen = [54, 64, 57]
+
+    mountainBlue = (83, 78, 90)
+    darkMountainBlue = [42, 40, 52]
+
+    light = [220, 216, 201]
+    dark = [18, 22, 27]
+    accentOrange = [155, 110, 83]
+
+    debugRed = [255, 96, 141]
+
+
+# Defining some more variables to use in the game loop
+oscillating_random_thing = 0
+ShakeCounter = 0
+toggle = True
+click = False
+
+tileSize = 3
+TH = TileHandler(screen_width, screen_height, tileSize, Cols, 0.51, 0.54, 100, font=montserratRegular15)
+
+# ---------------- Main Game Loop
+last_time = time.time()
+running = True
+while running:
+
+    # ---------------- Reset Variables and Clear screens
+    mx, my = pygame.mouse.get_pos()
+    mx, my = mx / scaleDownFactor, my / scaleDownFactor
+    screen.fill(Cols.oceanBlue)
+    screen2.fill(Cols.oceanBlue)
+    screenT.fill((0, 0, 0, 0))
+    screenUI.fill((0, 0, 0, 0))
+    dt = time.time() - last_time
+    dt *= fps
+    last_time = time.time()
+    timer -= 1 * dt
+    shake = [0, 0]
+    oscillating_random_thing += math.pi / fps * dt
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            click = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            click = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            if event.key == pygame.K_SPACE:
+                toggle = not toggle
+        if event.type == pygame.KEYUP:
+            pass
+
+    TH.draw(screen2, showArrows=False, showDebugOverlay=False, showWaterLand=False)
+
+    # ---------------- Updating Screen
+    if toggle:
+        items = {round(clock.get_fps()): None, }
+        for _, label in enumerate(items.keys()):
+            string = str(label)
+            if items[label] is not None:
+                string = f"{items[label]}: " + string
+            drawText(screenUI, Cols.debugRed, montserratRegularAdaptive, 5, screen_height - (30 + 25 * _) / (scaleDownFactor ** (1 / 1.8)), string, Cols.dark, int(3 / scaleDownFactor) + int(3 / scaleDownFactor) < 1, antiAliasing=False)
+        pygame.mouse.set_visible(False)
+        pygame.draw.circle(screenUI, Cols.dark, (mx + 1, my + 1), 2, 1)
+        pygame.draw.circle(screenUI, Cols.light, (mx, my), 2, 1)
+    screen.blit(pygame.transform.scale(screen2, (screen_width * scaleDownFactor, screen_height * scaleDownFactor)), (shake[0], shake[1]))
+    screen.blit(pygame.transform.scale(screenT, (screen_width * scaleDownFactor, screen_height * scaleDownFactor)), (shake[0], shake[1]))
+    screen.blit(pygame.transform.scale(screenUI, (screen_width * scaleDownFactor, screen_height * scaleDownFactor)), (0, 0))
+    pygame.display.update()
+    clock.tick(fps)
