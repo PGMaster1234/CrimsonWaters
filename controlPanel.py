d2efb41cd0f2f0a63236730da0b3dc86298090a4
Pranav <pxg082007@gmail.com>
2025-04-30 09:06:02 -0500

    Panel to store all spawning parameters

---
+++ controlPanel.py
@@ -0,0 +1,12 @@
+class ResourceInfo:
+    numResources = 5
+
+    woodSpawnRate = 0.004
+    stoneSpawnRate = 0.01
+    ironSpawnRate = 0.003
+    pineSpawnRate = 0.003
+    amberSpawnRate = 0.02
+
+
+class StructureInfo:
+    harborSpawnRate = 0.015
